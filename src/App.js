7e3a0bb20339ba9795dd117868de2bbb1b06698e
mmiiruu <147244523+mmiiruu@users.noreply.github.com>
2024-11-13 18:49:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,42 +11,35 @@ import Candidate from "./Candidate";
 import AdminPage from "./AdminPage";
 
 function App() {
-  const [user, setUser] = useState(null); // สถานะล็อกอินของผู้ใช้
+  const [user, setUser] = useState(null); 
 
   const handleLogin = (loggedInUser) => {
-    setUser(loggedInUser); // ตั้งค่าสถานะล็อกอิน
+    setUser(loggedInUser); 
   };
 
   return (
     <Router>
       <Routes>
-        {/* เส้นทางไปหน้าล็อกอิน */}
         <Route
           path="/login"
           element={
             !user ? <Login onLogin={handleLogin} /> : <Navigate to="/year" />
           }
         />
-
-        {/* เส้นทางไปหน้าเลือกปีการเลือกตั้ง */}
         <Route
           path="/year"
           element={user ? <ElectionYear /> : <Navigate to="/login" />}
         />
 
-        {/* เส้นทางแบบไดนามิกสำหรับหน้า /year/:year */}
         <Route
           path="/year/:year"
           element={user ? <Candidate /> : <Navigate to="/login" />}
         />
 
-        {/* เส้นทางไปหน้า Admin */}
         <Route
           path="/admin"
           element={user ? <AdminPage /> : <Navigate to="/login" />}
         />
-
-        {/* เส้นทางเริ่มต้น */}
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
